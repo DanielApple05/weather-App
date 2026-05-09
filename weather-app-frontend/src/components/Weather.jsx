1be442cc6c0363c9a8455fe26e69ef1fce8e7514
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-05-09 02:17:37 +0100

    Refactor Weather component layout for improved responsiveness and styling consistency

--- weather-app-frontend/src/components/Weather.jsx
+++ weather-app-frontend/src/components/Weather.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-// import Logo from '/logo.svg'
-
 
 const Weather = () => {
   return (
     <div>
-      <div className=' bg-[#050620] h-screen text-gray-300 p-10 '>
+      <div className=' bg-[#050620] text-gray-300 xl:p-10 p-5 min-h-screen '>
         <div className='flex justify-between '>
-          <div>
-            <img src='/images/logo.svg' alt="" />
-          </div>
+          <img src='/images/logo.svg' alt="" className='xl:w-40 w-30' />
           <div className='flex items-center justify-center bg-[#2F2D52] p-2 rounded-lg gap-2'>
             <img src="/images/icon-units.svg" alt="" />
             <p>
@@ -18,14 +14,14 @@ const Weather = () => {
             <img src="/images/icon-dropdown.svg" alt="" />
           </div>
         </div>
-        <div className='flex flex-col items-center justify-center gap-10'>
-          <h1 className='text-[35px]'>
+        <div className='flex flex-col items-center justify-center gap-10 xl:mt-0 mt-10'>
+          <h1 className='xl:text-[35px] text-[20px]'>
             How's the sky looking today?
           </h1>
-          <div className='flex gap-5 w-6/12 '>
-            <div className='flex bg-[#2F2D52] items-center p-3 rounded-lg flex-1 '>
-              <img src="/images/icon-search.svg" alt="" className='w-5' />
-              <input type="text" className='outline-none' placeholder='Search for a place...' />
+          <div className='flex gap-5 xl:w-6/12 w-full '>
+            <div className='flex bg-[#2F2D52] items-center p-3 rounded-lg flex-1  '>
+              <img src="/images/icon-search.svg" alt="" className='w-5 ' />
+              <input type="text" className='outline-none pl-2' placeholder='Search for a place...' />
             </div>
             <div>
               <p className='p-3 bg-[#030578] rounded-lg'>
@@ -34,9 +30,9 @@ const Weather = () => {
             </div>
           </div>
         </div>
-        <div className='flex mt-20 justify-between '>
-          <div className='w-7/12 grid gap-5 '>
-            <div className='flex justify-between items-center p-8 rounded-2xl bg-cover h-full bg-no-repeat' style={{ background: 'url(/images/bg-today-large.svg)'}} >
+        <div className='xl:flex flex-col mt-10 justify-between gap-5  '>
+          <div className=' gap-5 flex flex-col xl:w-7/12 w-full flex-1 '>
+            <div className='flex justify-between items-center p-8 rounded-2xl min-h-60 bg-cover bg-no-repeat bg-center bg-[url(/images/bg-today-large.svg)]'>
               <div className=''>
                 <p>Location</p>
                 <p>current date</p>
@@ -46,20 +42,20 @@ const Weather = () => {
                 <p>degree</p>
               </div>
             </div>
-            <div className='flex gap-5'>
-              <div className='grid bg-[#2F2D52] w-35 p-5 rounded-lg gap-2 '>
+            <div className='xl:flex grid grid-cols-2 justify-between gap-5'>
+              <div className='grid bg-[#2F2D52] w-full p-5 rounded-lg gap-2 '>
                 <span> Feels Like</span>
                 <span>0000</span>
               </div>
-              <div className='grid bg-[#2F2D52] w-35 p-5 rounded-lg gap-2'>
+              <div className='grid bg-[#2F2D52] w-full p-5 rounded-lg gap-2'>
                 <span> Humidity</span>
                 <span>percentage</span>
               </div>
-              <div className='grid bg-[#2F2D52] w-35 p-5 rounded-lg gap-2 '>
+              <div className='grid bg-[#2F2D52] w-full p-5 rounded-lg gap-2 '>
                 <span> Wind</span>
                 <span>distance</span>
               </div>
-              <div className='grid bg-[#2F2D52] w-35 p-5 rounded-lg gap-2 '>
+              <div className='grid bg-[#2F2D52] w-full p-5 rounded-lg gap-2 '>
                 <span> precipitation</span>
                 <span>mm</span>
               </div>
@@ -67,18 +63,19 @@ const Weather = () => {
             <div className='flex'>
               <p>Daily forecast</p>
               <div>
-              days of the week
+                days of the week
               </div>
             </div>
           </div>
-          <div className='w-4/12 flex flex-col items-center'>
-            <div className='flex items-center'>
-              <p>Hourly forecast</p>
-              <div className='flex p-2 items-center bg-[#3B3966] gap-2 rounded-lg'>
+          <div className='xl:w-4/12 w-full flex flex-col bg-[#2F2D52] rounded-lg'>
+            <div className='flex justify-between p-5 items-center '>
+              <p className='text-start'>Hourly forecast</p>
+              <div className='flex p-2 items-center bg-[#3B3966] rounded-lg gap-2'>
                 <p>Days</p>
                 <img src="/images/icon-dropdown.svg" alt="" />
               </div>
             </div>
+
           </div>
         </div>
       </div>
